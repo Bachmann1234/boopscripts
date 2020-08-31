3772eff4b2343c774b5c018324b573e1570b48a0
Matt Bachmann <bachmann.matt@gmail.com>
2020-08-31 19:47:48 -0400

    Add the json doc so I dont have to remake it each time

--- curlToPython_orig.js
+++ curlToPython_orig.js
@@ -4,12 +4,20 @@ const curlconverter = require('curlconverter');
  To bundle this:
  run this
  `browserify curlToPython_orig.js -o curlToPythonBundle.js`
- Then add the boop comment to the top of the bundle.
 
- Then add this line right below the comment
- const window = this;
+ Then add this json block as a block comment
+
+  {
+    "api":1,
+    "name":"Curl To Python",
+    "description":"Converts a curl command to a small python script",
+    "author":"Bachmann",
+    "icon":"metamorphose",
+    "tags":"curl,python,requests,convert"
+  }
+  Then add this line to the top of the script
+  const window = this;
  */
-
 global.main = function(state) {
     try {
         state.text = curlconverter.toPython(state.text);
